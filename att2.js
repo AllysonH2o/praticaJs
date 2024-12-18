96ab23e7a8613d0af077c389d84c58a43cfbaa1d
AllysonH2o <allyson.araujo.h2o@gmail.com>
2024-12-18 14:27:12 -0300

    atividade 2 js

---
+++ att2.js
@@ -0,0 +1,13 @@
+checkSpeed(72);
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint) console.log('ok');
+    else{
+        const points = Math.floor((speed - speedLimit)/5);
+        if (points >= 12) console.log("License suspended");
+        else console.log("points:", points);
+    }
+}
