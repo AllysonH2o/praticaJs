67c66131c04ac6b671bb5fb2477fec0bf92e9185
AllysonH2o <allyson.araujo.h2o@gmail.com>
2024-12-18 14:01:09 -0300

    1 atividade de js

---
+++ att.js
@@ -0,0 +1,10 @@
+function fizzBuzz(input) {
+    if (typeof input !== 'number') return 'not a number';
+    if (input%3 === 0 && input%5 === 0) return 'FizzBuzz';
+    if (input%3 === 0) return 'Fizz';
+    if (input%5 === 0) return 'Buzz';
+    return input;
+}
+
+const output = fizzBuzz(3);
+console.log(output);
