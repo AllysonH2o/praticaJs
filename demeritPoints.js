779a44ef8893b6683b16a80f8974dfbbebf6069d
AllysonH2o <allyson.araujo.h2o@gmail.com>
2024-12-18 14:56:48 -0300

    alteracao de nomes e melhorando codigos

--- demeritPoints.js
+++ demeritPoints.js
@@ -7,7 +7,7 @@ function checkSpeed(speed) {
     if (speed < speedLimit + kmPerPoint) {
         console.log('ok');
         return;
-    }
+    }//removi o else porque com o return sai do loop e assim diminui a indentação
     const points = Math.floor((speed - speedLimit)/5);
     if (points >= 12) console.log("License suspended");
     else console.log("points:", points);
