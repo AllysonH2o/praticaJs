fc287e2f81aa5693c5cd0ae5ade0cd1d50c64ee4
AllysonH2o <allyson.araujo.h2o@gmail.com>
2024-12-18 17:03:49 -0300

    corrigindo e melhorando o codigo de primeNumber

--- primeNumber.js
+++ primeNumber.js
@@ -1,17 +1,13 @@
 showPrimes(20);
 
 function showPrimes(limit) {
-    for (let number = 1; number <= limit; number++){
-        let prime = isPrisme(number);
-
-        if(prime) console.log(number);
-    }        
+    for (let number = 1; number <= limit; number++)
+        if(isPrime(number)) console.log(number);   
 }
 
-function isPrisme(number) {
-    for(let i = 2; i < number; i++){
+function isPrime(number) {
+    for(let i = 2; i < number; i++)
         if(number % i === 0)
             return false;
-    }
     return true;
 }
