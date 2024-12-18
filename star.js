313b3b8a5b47c93d8069ae9946f4d9c2b0b35880
AllysonH2o <allyson.araujo.h2o@gmail.com>
2024-12-18 16:20:10 -0300

    3 exercicio de js

---
+++ star.js
@@ -0,0 +1,11 @@
+showStars(10);
+
+function showStars(rows) {
+    for (let row = 1; row <= rows; row++){
+        let star = '';
+        for (let i = 0; i < row; i++)
+            star += '*';
+        console.log(star);
+    }
+
+}
